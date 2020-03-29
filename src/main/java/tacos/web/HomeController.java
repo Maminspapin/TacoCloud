0bea9f3e4bb24513d730c08e5e6624326e664f38
Maminspapin <y.lyulchenko@mail.ru>
2020-03-29 16:46:02 +0700

    Declaring a view controller instead of homeController

--- src/main/java/tacos/web/HomeController.java
+++ src/main/java/tacos/web/HomeController.java
@@ -3,12 +3,14 @@ package tacos.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+// Using WebConfig
+
+//@Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
+//    @GetMapping("/")
+//    public String home() {
+//        return "home";
+//    }
 
 }
