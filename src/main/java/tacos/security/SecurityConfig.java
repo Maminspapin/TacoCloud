177cec17fe20b37e03261539d947748a4bc6e9bb
Maminspapin <y.lyulchenko@mail.ru>
2020-06-22 21:48:46 +0700

    Added user

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -6,40 +6,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Autowired
-//    DataSource dataSource;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("Yuri")
-                    .password(passwordEncoder().encode("123"))
-                    .authorities("USER_ROLE")
-                .and()
-                    .withUser("Maria")
-                    .password(passwordEncoder().encode("234"))
-                    .authorities("USER_ROLE");
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
     }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication().dataSource(dataSource)
-//                .usersByUsernameQuery("select username, password, enabled from Users where username = ?")
-//                .authoritiesByUsernameQuery("select username, authority from UserAuthorities where username = ?")
-//        .passwordEncoder(new BCryptPasswordEncoder());
-//    }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
