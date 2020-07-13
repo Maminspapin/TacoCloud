8448dcd54ee2cb1c7611d53a9a7d20926d93c629
Maminspapin <y.lyulchenko@mail.ru>
2020-07-13 22:37:27 +0700

    Fix

--- src/main/java/tacos/entity/User.java
+++ src/main/java/tacos/entity/User.java
@@ -39,7 +39,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority("USER_ROLE"));
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
